e9875d3c564455f09d641560ba08ec373671b05c
Sneha Chourey <snehachourey@gmail.com>
2026-06-27 23:08:59 +0530

    fix: auto create upload directory on server

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,8 +1,16 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
+
+// folder automatically create karo
+const uploadDir = path.join('.', 'public', 'temp');
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join('.','uploads','temp'))
+    cb(null, uploadDir)
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
